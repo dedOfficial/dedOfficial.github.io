3a49e6fc4f645d22411a5f47b2bfe32681f59189
dedOfficial <77498473+dedOfficial@users.noreply.github.com>
2021-04-10 18:31:12 +0300

    fixed mobile adaptation

--- pulse/js/script.js
+++ pulse/js/script.js
@@ -105,12 +105,12 @@ $(document).ready(function(){
         event.preventDefault();
         $.ajax({
             type: 'POST',
-            url: '../mailer/smart.php',
+            url: 'mailer/smart.php',
             data: $(this).serialize()
         }).done(function() {
             $(this).find('input').val("");
             $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
+            $('.overlay, #thanks').fadeIn();
             $('form').trigger('reset');
         });
         return false;
