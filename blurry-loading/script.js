2be4d63f023cf2f4b0f80cd1d397719f223be2c8
dedOfficial <77498473+dedOfficial@users.noreply.github.com>
2021-05-22 08:54:05 +0300

    fixed blurry-loading

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -1,20 +1,23 @@
-const scale = (num, inMin, inMax, outMin, outMax) => {
-    return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
-
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
 let load = 0;
-let int = setInterval(bluring, 30);
 
-function bluring() {
-    load++;
-    if (load > 99) {
-        clearInterval(int);
-    }
-    loadText.innerText = `${load}%`;
-    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+let timeId = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(timeId);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
